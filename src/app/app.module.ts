3d8af5ede30ea84f08f391359a1a2bad409b5785
Eduardo Baron <jmendoza2486@gmail.com>
2018-07-26 21:31:29 -0500

    se crea version de alerts y nav controller entre paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { FormsModule } from '@angular/forms';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { InicioPage } from '../pages/inicio/inicio';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    InicioPage
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    InicioPage
   ],
   providers: [ //logica del negocio, bd, 
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
+import { InicioPage } from './../inicio/inicio';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  usuario : string = "";
+  pagina = InicioPage;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private NavParams: NavParams ,private navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Login',
+      subTitle: 'Sesión iniciada...Hola: ' + this.usuario,
+      buttons: [{
+        text: 'Iniciar',
+        handler: data => {
+          console.log(data);
+          this.navCtrl.push(this.pagina,{ 'nombre':this.usuario });
+        }
+      }]
+    });
+    console.log('recibe: ' + this.usuario);
+    alert.present();
+  }
+
+  showAlertRecuperar() {
+    let alert = this.alertCtrl.create({
+      title: 'Recuperar Contraseña',
+      subTitle: 'Escribe tu correo para recuperar tu contraseña.',
+      inputs : [{
+          placeholder: 'Correo electrónico'
+        },
+        {
+          placeholder: 'Correo electrónico2'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Enviar correo',
+          handler: data => {
+            console.log(data);
+            this.showAlertConfirmaRecuperar(data);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  showAlertConfirmaRecuperar(data) {
+    let alert = this.alertCtrl.create({
+      title: 'Recuperar Contraseña',
+      subTitle: 'Acabamos de enviarte un mensaje a tu correo: ' + data[0] + ', ' + data[1],
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
