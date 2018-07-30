e522b78b1d1ce8f819d8460cf9d5ecba6dad6702
Jaime <jmendoza2486@gmail.com>
2018-07-30 15:11:00 -0500

    Se termina validacion de campos de pagina registro, confirmacion y regreso pop a pagina inicio

--- src/pages/registro/registro.ts
+++ src/pages/registro/registro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the RegistroPage page.
@@ -13,9 +14,80 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'registro.html',
 })
 export class RegistroPage {
-  nombre: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.nombre = this.navParams.get('nombre');
+  nombre: string = "";
+  apellido: string = "";
+  usuario: string = "";
+  contrasena: string = "";
+  conf_contrasena: string = "";
+  email: string = "";
+  conf_email: string = "";
+  msj_obligatorio: string = "Campo obligatorio";
+  msj_confirmacion: string = "No coinciden los valores";
+  alertas: boolean[] = [false,false,false,false,false,false,false];
+  registro: boolean = true;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    //this.nombre = this.navParams.get('nombre');
+  }
+
+  reiniciaEstadosAlertas(){
+    this.alertas = [false,false,false,false,false,false,false];    
+    this.registro = true;
+  }
+
+  validaRegistro(){
+    if(this.nombre == ""){
+      this.alertas[0]=true;
+      this.registro = false;
+    }
+    if(this.apellido == ""){
+      this.alertas[1]=true;
+      this.registro = false;
+    }
+    if(this.usuario == ""){
+      this.alertas[2]=true;
+      this.registro = false;
+    }
+    if(this.contrasena == ""){
+      this.alertas[3]=true;
+      this.registro = false;
+    }
+    if(this.conf_contrasena == "" || this.contrasena !== this.conf_contrasena){
+      this.alertas[4]=true;
+      this.registro = false;
+    }
+    if(this.email == ""){
+      this.alertas[5]=true;
+      this.registro = false;
+    }
+    if(this.conf_email == "" || this.email !== this.conf_email){
+      this.alertas[6]=true;
+      this.registro = false;
+    }    
+  }
+
+  confirmaRegistro(){
+    if(this.registro){
+      let alert = this.alertCtrl.create({
+        title: 'Registro',
+        subTitle: 'Registro correcto ' + this.nombre.toUpperCase() + ', ya puedes iniciar sesión.',
+        buttons: [
+          {
+            text: 'OK',
+            handler:()=>{
+              this.navCtrl.pop();
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
+  }
+
+  creaRegistro(){
+    this.reiniciaEstadosAlertas();
+    this.validaRegistro();
+    this.confirmaRegistro();
   }
 
   ionViewDidLoad() {
